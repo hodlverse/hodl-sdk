78bfea2e285a573c4deb939b19820d469ccb9dd6
Hodl Engineer <hodl.engineer@gmail.com>
2021-12-02 22:53:15 +0100

    remove  dist

--- dist/constants/tokens.d.ts
+++
@@ -1,7 +0,0 @@
-import { Token } from "../entities/Token";
-export declare const WETH9: {
-    [chainId: number]: Token;
-};
-export declare const WNATIVE: {
-    [chainId: number]: Token;
-};
--- dist/enums/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { ChainId } from "./ChainId";
-export { Rounding } from "./Rounding";
-export { TradeType } from "./TradeType";
-export { KashiAction } from "./KashiAction";
